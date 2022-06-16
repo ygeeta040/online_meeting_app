d78f5ec87ac67429ca973cb1466b14d6b5f98803
ygeeta040 <ygeeta040@gmail.com>
2022-06-16 12:20:29 +0530

    first commit

---
+++ client/build/precache-manifest.f9bcb5ae79dbc930e7ad924547d3d6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3880a2ece9364876ee941361f250183e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c17fc969dc647b988a0",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "24610bbe36c43c572d4c",
+    "url": "/static/js/2.453347d1.chunk.js"
+  },
+  {
+    "revision": "4cd109a73838e7185759944dc7a7b9a9",
+    "url": "/static/js/2.453347d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c17fc969dc647b988a0",
+    "url": "/static/js/main.2acaf961.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
